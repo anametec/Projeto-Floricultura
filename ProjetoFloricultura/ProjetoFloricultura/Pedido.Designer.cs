9c60729137a6a4a92f871daa039a3b8dd9df7f5e
Ana <anacarolinathbg@gmail.com>
2025-09-04 17:18:47 -0300

    Subindo para o github da Paula

--- ProjetoFloricultura/ProjetoFloricultura/Pedido.Designer.cs
+++ ProjetoFloricultura/ProjetoFloricultura/Pedido.Designer.cs
@@ -250,7 +250,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Magenta;
+            this.BackColor = System.Drawing.Color.DeepPink;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.btnSair);
             this.Controls.Add(this.btnCalcular);
